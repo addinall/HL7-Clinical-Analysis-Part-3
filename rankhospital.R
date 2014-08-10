149f7638dd694ff989b3628e89dbd14f45ccbfe1
addinall <addinall@addinall.net>
2014-08-11 06:57:18 +1000

    Might as well get this one finished

---
+++ rankhospital.R
@@ -0,0 +1,13 @@
+## CAPTAIN SLOG
+## vim: set expandtab tabstop=4 shiftwidth=4 autoindent smartindent:
+## File         : rankhospital.R
+## System       : Assignment 3 (Autograded)
+## Date         : 12/08/2014
+## Author       : Mark Addinall
+## Synopsis     : This file is part of the course work
+##                assignments for the Johns Hopkins
+##                series of Data Science units.
+##                This unit is R Programming
+##
+
+
