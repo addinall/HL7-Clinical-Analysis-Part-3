396f937531500f67b3f9dd4d123f3e3d44d22aaf
addinall <addinall@addinall.net>
2014-08-11 14:08:54 +1000

    Bad State and bad outcome finished

--- best.R
+++ best.R
@@ -10,3 +10,28 @@
 ##                This unit is R Programming
 ##
 
+## -------------------------------
+best <- function(state, outcome) {
+## Read outcome data
+## Check that state and outcome are valid
+## Return hospital name in that state with lowest 30-day death
+## rate
+
+outcomes <- c("heart attack", "heart failure", "pneumonia")
+
+if (! outcome %in% outcomes) {
+    stop("invalid outcome")
+}
+
+hospitals <- read.csv("data/hospital-data.csv")
+
+states <- unique(hospitals[, "State"])
+
+if (! state %in% states) {
+    stop("invalid state")
+}
+
+
+
+
+}
