b27c39fe92514e617df96383363a20211f3e5e32
addinall <addinall@addinall.net>
2014-08-18 21:02:57 +1000

    Final week part two finished - rankhospital.R

--- rankhospital.R
+++ rankhospital.R
@@ -49,18 +49,19 @@ rankhospital <- function(state, outcome, num = "best") {
 ## Return hospital name in that state with the given rank
 ## 30-day death rate
 
-if (num == "best" {
-    num = "1"
-} else if {
-    (num == "worst") {
-        num = "-1"
-}
+if (num == "best") {            ## mixing argument types, tsk!
+    num <- "1"                  ## who writes these specs?
+} else if (num == "worst") {    ## at the end of the program
+        num <- "-1"             ## if we find that the rank
+}                               ## requested is LESS than 0,
+                                ## then we want the LAST record
+                                ## in the set to be returned
 
 suppressWarnings(num <- as.numeric(num))
 
-if (num == NA) {
-    return(num)
-}
+if (is.na(num)) {               ## this shouldn't happen,
+    return(num)                 ## BUT, with a very loosely
+}                               ## type language, num COULD be "BUM"
 
 
 outcomes <- c("heart attack", "heart failure", "pneumonia")
@@ -80,6 +81,7 @@ if (! state %in% states) {
 
 ## I am just getting rid of a big ugly column name here.
 ## way too much typing and prone to error
+
 if (outcome == "heart attack") {
     deaths <- "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack" 
 } else if (outcome == "heart_failure") {
@@ -88,38 +90,47 @@ if (outcome == "heart attack") {
     deaths <-"Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia"}
 
 ## sort on two column to resolve ties in scores for first place
+
 sortnames       <- c(deaths, "Hospital.Name")
 
-## trim the dataset down from 46 column to 3 that we need
-outcome_data    <- outcome_data[, c("Hospital.Name", "State", deaths)]
+## trim the dataset down from 46 column to 4 that we need
+
+outcome_data    <- outcome_data[, c("Provider.Number", "Hospital.Name", "State", deaths)]
 
 ## and dump all but the state we are interested in
-outcome_data    <- subset(outcome_data, outcome_data[, 2] == state)
+
+outcome_data    <- subset(outcome_data, outcome_data[, 3] == state)
 
 number_hospitals <- nrow(outcome_data)
 
-if (number_hospitals < num {
+## go awy if a silly ROOLY big number record
+## is requested
+
+if (number_hospitals < num) {
     return(NA)
 }
 
 
 ## we know, we WANT to force the NA converstions
-suppressWarnings(outcome_data[,3] <- as.numeric(outcome_data[, 3]))
+
+suppressWarnings(outcome_data[,4] <- as.numeric(outcome_data[, 4]))
 
 ## dump the NAs
 ## this came about because the people that put the database
 ## together mixed numbers with "Too tichy" so R turns the
 ## whole database into characters.  We need to turn some
 ## of them back into numbers for the sort.
+
 outcome_data    <- na.omit(outcome_data)
 
 ## sort by rank then name
+
 outcome_data <- outcome_data[do.call("order", outcome_data[sortnames]),]
 
-## da da!  The WINNER!!!
-print(outcome_data[1, 1])
+if (num == -1) {                ## worst rank
+    num <- nrow(outcome_data)
+}
 
-
-
+print(outcome_data[num, 2])
 
 }
